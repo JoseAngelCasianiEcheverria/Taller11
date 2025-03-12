77aaf40c5aae5e6134b4e1bf498bc67c7a4dbbec
JoseAngelCasianiEcheverria <joseangelcasianiecheverria@gmail.com>
2025-03-11 19:29:13 -0500

    Ejercicio4

---
+++ Ejercicio4/src/main/java/Vehiculos/Bicicleta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Bicicleta extends Vehiculo{
+    
+    @Override
+    void moverse() {
+        System.out.println("La bicicleta esta moviendose por la ciclovia");
+    }
+}
---
+++ Ejercicio4/src/main/java/Vehiculos/Ejercicio4.java
@@ -0,0 +1,15 @@
+
+
+package Vehiculos;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        
+        Vehiculo vehiculo1 = new Coche();
+        Vehiculo vehiculo2 = new Bicicleta();
+        
+        vehiculo1.moverse();
+        vehiculo2.moverse();
+    }
+}
